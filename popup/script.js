dbe21c83df13dd7ed6ede0afae128a6432724f4b
simplon-gregoryg <grexor@hotmail.com>
2016-03-15 15:49:04 +0100

    preparation du dossier

---
+++ popup/script.js
@@ -0,0 +1,19 @@
+var element = document.getElementById('scriptonglet');
+
+element.addEventListener('click', function() {
+
+  chrome.tabs.query({'title': 'La Fabrique à Innovations | Donnez vie à vos idées !'}, (tabs) => {
+    if (tabs.length === 0) {
+      // There is no catgif tab!
+      chrome.tabs.create({'url': 'http://lafabriqueainnovations.com/' , 'active': true});
+    } else {
+      // Do something here…
+      chrome.tabs.query({'title': 'La Fabrique à Innovations | Donnez vie à vos idées !', 'active': true}, (active) => {
+        if (active.length === 0) {
+          chrome.tabs.update(tabs[0].id, {'active': true});
+        }
+      });
+    }
+  });
+
+}, false);
