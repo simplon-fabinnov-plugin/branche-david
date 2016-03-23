9a36b32a2017ed495a8075fb30885840122f2af6
simplon-davidt <david@tarczewski.fr>
2016-03-23 11:36:26 +0100

    Revert "deplacement de front-end dans popup"
    This reverts commit fc6a2f1caa2b90e438337fc3d30183e2a81d1b73.

--- plugin/popup/script.js
+++ plugin/popup/script.js
@@ -1,16 +1,16 @@
-//Ajoute un evenement sur le clic du lien (redirection vers onglet deja ouvert)
-
-var element = document.querySelector('#scriptOnglet');
+var element = document.getElementById('scriptonglet');
 
 element.addEventListener('click', function() {
 
   chrome.tabs.query({'title': 'La Fabrique à Innovations | Donnez vie à vos idées !'}, tabs => {
     if (tabs.length === 0)
     {
+      // There is no catgif tab!
       chrome.tabs.create({'url': 'http://lafabriqueainnovations.com/', 'active': true});
     }
     else
     {
+      // Do something here…
       chrome.tabs.query({'title': 'La Fabrique à Innovations | Donnez vie à vos idées !', 'active': true}, active => {
         if (active.length === 0)
         {
@@ -19,4 +19,5 @@ element.addEventListener('click', function() {
       });
     }
   });
+
 }, false);
